095ee2c8de61c7ab7975bd4726e6d93baf328937
Yongkang He <he@yongkang.cloud>
2022-07-15 22:04:47 +0800

    Update aroprep.sh

--- aroprep.sh
+++ aroprep.sh
@@ -1,3 +1,4 @@
+echo "-------Install oc client, create an app registration"
 cd ~
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
 mkdir openshift
@@ -5,9 +6,12 @@ tar -zxvf openshift-client-linux.tar.gz -C openshift
 echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc && source ~/.bashrc
 rm openshift-client-linux.tar.gz
 
+AZURE_SUBSCRIPTION_ID=$(az account list --query "[?isDefault][id]" --all -o tsv)
+az ad sp create-for-rbac --name https://aks4yong1-k10-app --role Contributor --scopes /subscriptions/$AZURE_SUBSCRIPTION_ID -o json > aks4yong1app
+
 clear
 
+echo "-------oc installed, App Registration has been created"
 echo "" | awk '{print $1}'
 echo "You are ready to deploy now!"
 echo "" | awk '{print $1}'
-
