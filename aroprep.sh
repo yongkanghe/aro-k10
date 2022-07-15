453b3d46d488c193d6b77df3a995977b20cc83ff
Yongkang He <he@yongkang.cloud>
2022-07-15 21:49:37 +0800

    Create aroprep.sh

---
+++ aroprep.sh
@@ -0,0 +1,6 @@
+cd ~
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
+mkdir openshift
+tar -zxvf openshift-client-linux.tar.gz -C openshift
+echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc && source ~/.bashrc
+rm openshift-client-linux.tar.gz
